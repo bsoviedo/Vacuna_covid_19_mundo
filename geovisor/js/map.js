e5b1ede2e6a7247a3d8fbf2bb09b60e83b7d889e
bsoviedoy <63557478+bsoviedoy@users.noreply.github.com>
2021-03-30 19:25:20 -0500

    Add files via upload

--- geovisor/js/map.js
+++ geovisor/js/map.js
@@ -1,76 +1,44 @@
 
 const map= L.map('map', { zoomControl: false }).setView([0, 0 ], 2.4);
 
-//var base= L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',).addTo(map);
-
-/*
-var base= L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-
-base.addTo(map)*/
+// Documentación API trackeo de vacunación https://documenter.getpostman.com/view/14021091/TzCJgVTT?fbclid=IwAR0Dzn0jIYZt-PkTjhA4EAI_nQNjhkBSDY5kWr8gUFt2_UTbcyJ6S8gWpK0 
 
 //acá se lee el layer del geojson
 
+const covid_vaccine= [];
 
+async function onEachFeature (feature, layer) {
+  const search= feature.properties.name;
 
-
-
-
-async function onEachFeature(feature, layer) {
-        const search= feature.properties.name;
-
-        const res= await fetch(`https://api.thecovidvaccines.com/api/v1/countries/${search}`);
-        const data= await res.json();
-        const vaccine= data.data
-
-       
-
-        if (typeof (vaccine) == 'undefined'){
-          layer.bindPopup('No hay datos disponibles');
-         
-        }
-        else{
-          layer.bindPopup(
-            "<h1>" + search + "</h1>" +
-            "Vacunados del día = " + vaccine[0].daily_vaccinations +"<br>" +
-          "Fecha última actualización= " + vaccine[0].date + "<br>" +
-          "Total vacunados= " + vaccine[0].people_vaccinated + "<br>" +
-          "Vacunados por cada 100 Habitantes= " + vaccine[0].people_vaccinated_per_hundred + "<br>" 
-          
-          
-          );
-         
-        }
- 
-  
-  };
-
-
-
-
- function MyStyle(feature){
+  const res= await fetch(`https://api.thecovidvaccines.com/api/v1/countries/${search}`);
+  const data= await res.json();
+  const vaccine= data.data
 
 
   if (typeof (vaccine) == 'undefined'){
-    console.log('holi, estoy indefinido')
-  }
-  else{
-    console.log('holi, estoy definido')
-
+    console.log('err')
+    layer.bindPopup('No hay datos disponibles');
    
   }
+  else{
+     layer.bindPopup(
+      "<h1>" + search + "</h1>" +
+      "Vacunados del día = " + vaccine[0].daily_vaccinations +"<br>" +
+    "Fecha última actualización= " + vaccine[0].date + "<br>" +
+    "Total vacunados= " + vaccine[0].people_vaccinated + "<br>" +
+    "Vacunados por cada 100 Habitantes= " + vaccine[0].people_vaccinated_per_hundred + "<br>" 
+    ); 
+
+    covid_vaccine.push(data);
+  }
+}
 
 
 
-}  
+console.log(covid_vaccine);
+
 
 
   L.geoJson(data,{
-    style: MyStyle,
-    onEachFeature: onEachFeature,
+    onEachFeature: onEachFeature
   }).addTo(map);
-
-
-
-
-
-
