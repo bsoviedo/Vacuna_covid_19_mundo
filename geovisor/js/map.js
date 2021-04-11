9b4ae57a1ae333bb61559774ce6700f711442347
bsoviedoy <63557478+bsoviedoy@users.noreply.github.com>
2021-04-11 14:57:02 -0500

    v.2 Actualiza visualización información

--- geovisor/js/map.js
+++ geovisor/js/map.js
@@ -1,5 +1,5 @@
 
-const map= L.map('map', { zoomControl: false }).setView([0, 0 ], 2.4);
+const map= L.map('map', { zoomControl: false, scrollWheelZoom: false }).setView([0, 0 ], 2.4);
 
 // Documentación API trackeo de vacunación https://documenter.getpostman.com/view/14021091/TzCJgVTT?fbclid=IwAR0Dzn0jIYZt-PkTjhA4EAI_nQNjhkBSDY5kWr8gUFt2_UTbcyJ6S8gWpK0 
 
@@ -10,27 +10,31 @@ const covid_vaccine= [];
 async function onEachFeature (feature, layer) {
   const search= feature.properties.name;
 
-  const res= await fetch(`https://api.thecovidvaccines.com/api/v1/countries/${search}`);
+  const res= await fetch(`https://api.thecovidvaccines.com/api/v1/countries/${search}`); 
   const data= await res.json();
   const vaccine= data.data
 
 
-  if (typeof (vaccine) == 'undefined'){
-    console.log('err')
-    layer.bindPopup('No hay datos disponibles');
-   
-  }
-  else{
-     layer.bindPopup(
-      "<h1>" + search + "</h1>" +
-      "Vacunados del día = " + vaccine[0].daily_vaccinations +"<br>" +
-    "Fecha última actualización= " + vaccine[0].date + "<br>" +
-    "Total vacunados= " + vaccine[0].people_vaccinated + "<br>" +
-    "Vacunados por cada 100 Habitantes= " + vaccine[0].people_vaccinated_per_hundred + "<br>" 
-    ); 
+  var div = L.DomUtil.create('div', 'highchart');
 
-    covid_vaccine.push(data);
+
+  layer.on('click', function (e) {
+    if (typeof (vaccine) == 'undefined'){
+      document.getElementById("info").innerHTML=  " <h1>" + search + "</h1> <br>" + '<h2> No hay datos disponibles<h2> '
+    }
+    else{
+      document.getElementById("info").innerHTML = " <h1>" + search + "</h1>" +
+      "<table border=1> <tr> <td> Vacunados del día    <td>" + "<td>"+ vaccine[0].daily_vaccinations +" </td> </tr> " +
+    "<tr> <td> Fecha última actualización <td>" + "<td>"+ vaccine[0].date + "</td> </tr> " +
+    " <tr> <td> Total vacunados <td>" + "<td>" + vaccine[0].people_vaccinated + "</td> </tr> " +
+    "<tr> <td> Vacunados por cada 100 Habitantes <td>" + "<td>" + vaccine[0].people_vaccinated_per_hundred + "</td> </tr>  </table>" ;
   }
+    console.log(vaccine[0].date)
+
+
+  });
+
+
 }
 
 
@@ -39,6 +43,6 @@ console.log(covid_vaccine);
 
 
 
-  L.geoJson(data,{
+L.geoJson(data,{
     onEachFeature: onEachFeature
   }).addTo(map);
